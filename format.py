e0c79fcb38b710dacaa3ed8c94be3e50114bab47
NJCaminiti <njcaminiti@gmail.com>
2018-04-13 16:36:05 -0400

    Finalized scripts and completed project.

--- format.py
+++ format.py
@@ -6,6 +6,10 @@ raw = pd.read_csv('RESULTS.csv')
 # Reshaping the Dataframe and trimming the start and end years where data is missing 
 tidy = pd.pivot_table(raw, values = 'avg_temp', index= 'year', columns= 'city').loc[1750:2013]
 
+#check for and locate missing values
+print(tidy.isnull().any())
+print(tidy[tidy.isnull().any(axis=1)])
 
-
+#impute missing value
+tidy.loc[1780]['Baltimore'] = tidy.loc[1778]['Baltimore']
 
